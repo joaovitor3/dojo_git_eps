067719d56c5c29a440f19e1d3264e64bd33f5db2
Caio Fernandes <caiovf13@gmail.com>
2019-03-18 20:49:04 -0300

    Main function

--- calculadora.c
+++ calculadora.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 
-int soma(int &num1, int &num2){
+int sum(int num1, int num2){
   int result;
-  result = *num1 + *num2;
-  return result;}
-int sub(int a, int b) {
-	return a - b;
+  result = num1 + num2;
+  return result;
 }
 
-double div(double n1, double n2){
-	return n1/n2;
 
+
+int subtraction(int a, int b) {
+  int result;
+  result = a-b;
+	return result;
+}
+
+
+double divider(double n1, double n2){
+  double result;
+  result = n1/n2;
+	return result;
+}
+
+int main(){
+  int num1, num2, soma, subt;
+  double n1, n2, d;
+
+  num1 = 2;
+  num2 = 3;
+
+  n1 = 10.0;
+  n2 = 2.0;
+
+  soma = sum(num1, num2);
+  subt = subtraction(num2, num1);
+  d = divider(n1,n2);
+
+  return 0;
 }
