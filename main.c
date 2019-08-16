9217febcae888e3ae6678a4bdfb58a8349f190dc
Tiago Rodrigues Pereira <tiagorodriguesp2@gmail.com>
2019-08-15 22:10:09 -0300

    Adiciona função de soma em C

---
+++ main.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+int main{
+ int a, b, c;
+
+ printf("Coloque o primeiro numero\n";
+ scanf("%d", &a);
+
+ printf("Segundo numero\n");
+ scanf("%d", &b);
+
+
+ c = a + b;
+
+ printf("Soma deu  %d\n", c); 
+
+ return 0;
+
+}
